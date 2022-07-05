1c272c8460d8c23a9946f3b202ef10cb16258bd1
DavidOkulski <32730627+DavidOkulski@users.noreply.github.com>
2022-07-05 11:11:54 -0700

    Added missing features
    Co-authored-by: Nolan Kurylo <nolankurylo@users.noreply.github.com>

--- app/featureEngineer/featureEngineer.py
+++ app/featureEngineer/featureEngineer.py
@@ -1,7 +1,8 @@
-from pandas import DataFrame, get_dummies
+from pandas import DataFrame, get_dummies,read_json
 
 def pre_process(data) -> DataFrame:
     """Pre-processes JSON data for the ML model"""
+    df_p = read_json("data/preReqData.json")
 
     df = DataFrame(data)
     df["subjectCourse"] = df["subject"].astype(str)  + df["code"].astype(str)
@@ -9,10 +10,22 @@ def pre_process(data) -> DataFrame:
 
     df = df.reset_index(drop=True)
     for i in df.index:
+         # Get the list of prereqs for this course
+        preReqsIndex = df_p[df_p['course'] == df.at[i, "subjectCourse"]].reset_index()
+        preReqsList = preReqsIndex.at[0, "preReqs"]
+
+        # Set the number of pre reqs
+        df.at[i, "# prereqs"] = len(preReqsList)
+
         for j in df.index:
-                df.at[i,df.at[j, "subjectCourse"]] = 1
+            # Number of offerings of the pre reqs in the previous semester
+            if (df.at[j, "subjectCourse"] in preReqsList):
+                df.at[i, "# prereqs prev sem"] += 1
+
+            df.at[i,df.at[j, "subjectCourse"]] = 1
 
     df = get_dummies(df, columns=['subjectCourse','semester'])
     df.fillna(0, inplace=True, downcast="infer")
+    df.to_csv("feature.csv",index=False)
 
     return df
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-source venv/bin/activate
+source venv/Scripts/activate
 export FLASK_APP=setup.py
 flask run -h 0.0.0.0
