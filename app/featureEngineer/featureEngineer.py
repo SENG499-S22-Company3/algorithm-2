b6d669ce45e49666109b44e9447845fcc3b52655
davidokulski <davidokulski@uvic.ca>
2022-07-13 14:54:10 -0700

    Added feature to endpoint preprocess

--- app/featureEngineer/featureEngineer.py
+++ app/featureEngineer/featureEngineer.py
@@ -9,7 +9,7 @@ def pre_process(data) -> DataFrame:
 
     df = DataFrame(data)
     
-    df = df.assign(**{"# prereqs prev sem":0})
+    df = df.assign(**{"# prereqs prev sem":0,"# Offerings":1})
     df["subjectCourse"] = df["subject"].astype(str)  + df["code"].astype(str)
     df = df.drop(columns = ['subject', 'code'])
 
@@ -24,6 +24,12 @@ def pre_process(data) -> DataFrame:
         df.at[i, "# prereqs"] = len(preReqsList)
         
         for j in df.index:
+            if all([
+                str(df.at[i, "semester"]) == str(df.at[j, "semester"]),
+                df.at[i, "subjectCourse"] == df.at[j, "subjectCourse"]
+            ]):
+                df.at[i, "# Offerings"] = df.at[i, "# Offerings"] + 1
+
             # Number of offerings of the pre reqs in the previous semester
             if (df.at[j, "subjectCourse"] in preReqsList):
                 df.at[i, "# prereqs prev sem"] += 1
