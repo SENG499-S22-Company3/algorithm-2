f242fd405d74cfeab3c9d73e9bffdfdc7252deb2
Nolan Kurylo <44009838+nolankurylo@users.noreply.github.com>
2022-07-19 12:54:04 -0700

    Finished unit tests and their use cases

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -5,7 +5,8 @@ This script uses the unittest framework to implement unit tests for the endpoint
 import ast
 import json
 import unittest
-from .test_base import BaseCase
+import random
+from test_base import BaseCase
 
 
 class EndpointTests(BaseCase):
@@ -44,9 +45,42 @@ class EndpointTests(BaseCase):
         # Check response code
         self.assertEqual(response.status_code, 200)
 
-    def test_normal_course(self) -> None:
+    ####### UNIT TESTS MAIN USE CASES #######
+    def test_normal_course_case1(self) -> None:
         """ Tests returned capacity for courses we must make a prediction on (ECE/SENG/CSC) 
-            Case 1: input capacity is > 0 so output capacity == input capacity (unchanged)
+            Case 1: input capacity is > 0 so output capacity == input capacity (unchanged)"""
+        # Change this number to modify the number of randomly generated courses
+        number_of_test_courses = 100
+
+        courses = []
+        for _ in range(number_of_test_courses):
+            faked_course = self.fake.course(course_type="NORMAL")
+            course = {
+                "subject": faked_course["subject"],
+                "code": faked_course["code"],
+                "seng_ratio": self.fake.pyfloat(left_digits=1,
+                                                right_digits=2,
+                                                max_value=1,
+                                                positive=True),
+                "semester": self.fake.semester(),
+                "capacity": random.randint(1, 1000)
+            }
+            courses.append(course)
+
+        payload = json.dumps(courses)
+        response = self.app.post("/predict_class_size",
+                                 data=payload,
+                                 content_type="application/json")
+
+        # Convert response data to Python object
+        response_courses = ast.literal_eval(response.data.decode("UTF-8"))
+
+        # Check that the algorithm returns a positive capacity
+        for course in response_courses:
+            self.assertGreater(course["capacity"], 0)
+
+    def test_normal_course_case2(self) -> None:
+        """ Tests returned capacity for courses we must make a prediction on (ECE/SENG/CSC) 
             Case 2: input capacity == 0 so output capacity > 0 (predicted)"""
         # Change this number to modify the number of randomly generated courses
         number_of_test_courses = 100
@@ -74,14 +108,13 @@ class EndpointTests(BaseCase):
         # Convert response data to Python object
         response_courses = ast.literal_eval(response.data.decode("UTF-8"))
 
-        # Check that the algorithm does not return a negative capacity
+        # Check that the algorithm returns a postive capacity
         for course in response_courses:
             self.assertGreater(course["capacity"], 0)
 
-    def test_oos_course(self) -> None:
-        """ Tests returned capacity for courses we must make a prediction on (ECE/SENG/CSC) 
-            Case 1: input capacity is > 0 so output capacity == input capacity (unchanged)
-            Case 2: input capacity == 0 so output capacity > 0 (predicted)"""
+    def test_oos_course_case1(self) -> None:
+        """ Courses that are in our Program but are out of scope for our schedulator (ECON180, MATH122, etc.)
+            Case 1: input capacity is >= 0 so output capacity == input capacity (unchanged)"""
         # Change this number to modify the number of randomly generated courses
         number_of_test_courses = 100
 
@@ -96,8 +129,9 @@ class EndpointTests(BaseCase):
                                                 max_value=1,
                                                 positive=True),
                 "semester": self.fake.semester(),
-                "capacity": 0
+                "capacity": random.randint(0, 1000)
             }
+            course['input_capacity'] = course['capacity']
             courses.append(course)
 
         payload = json.dumps(courses)
@@ -108,15 +142,48 @@ class EndpointTests(BaseCase):
         # Convert response data to Python object
         response_courses = ast.literal_eval(response.data.decode("UTF-8"))
 
-        # Check that the algorithm does not return a negative capacity
+        # Check that the algorithm does not change the input capacity
         for course in response_courses:
-            self.assertEqual(course["capacity"], 0)
+            self.assertEqual(course["capacity"], course['input_capacity'])
 
+    def test_new_course_case1(self) -> None:
+        """ New courses that are being offered for the first time must have default capacity of 80 (CSC227)
+            Case 1: input capacity is > 0 so output capacity == input capacity (unchanged)"""
+        # Change this number to modify the number of randomly generated courses
+        number_of_test_courses = 100
 
-    def test_new_course(self) -> None:
-        """ Tests returned capacity for courses we must make a prediction on (ECE/SENG/CSC) 
-            Case 1: input capacity is > 0 so output capacity == input capacity (unchanged)
-            Case 2: input capacity == 0 so output capacity > 0 (predicted)"""
+        courses = []
+        for _ in range(number_of_test_courses):
+            faked_course = self.fake.course(course_type="NEW")
+            course = {
+                "subject": faked_course["subject"],
+                "code": faked_course["code"],
+                "seng_ratio": self.fake.pyfloat(left_digits=1,
+                                                right_digits=2,
+                                                max_value=1,
+                                                positive=True),
+                "semester": self.fake.semester(),
+                "capacity": 0,
+                "capacity": random.randint(1, 1000)
+            }
+            course['input_capacity'] = course['capacity']
+            courses.append(course)
+
+        payload = json.dumps(courses)
+        response = self.app.post("/predict_class_size",
+                                 data=payload,
+                                 content_type="application/json")
+
+        # Convert response data to Python object
+        response_courses = ast.literal_eval(response.data.decode("UTF-8"))
+
+        # Check that the algorithm does not change the input capacity
+        for course in response_courses:
+            self.assertEqual(course["capacity"],  course['input_capacity']) 
+
+    def test_new_course_case2(self) -> None:
+        """ New courses that are being offered for the first time must have default capacity of 80 (CSC227)
+            Case 2: input capacity == 0 so output capacity == 80 (default assigned)"""
         # Change this number to modify the number of randomly generated courses
         number_of_test_courses = 100
 
@@ -143,7 +210,7 @@ class EndpointTests(BaseCase):
         # Convert response data to Python object
         response_courses = ast.literal_eval(response.data.decode("UTF-8"))
 
-        # Check that the algorithm does not return a negative capacity
+        # Check that the algorithm assigns default 80 capacity
         for course in response_courses:
             self.assertEqual(course["capacity"], 80)    
 
