03ed81aea9a867efaf70071b8187b4148f2d29fa
Scott Theriault <scottlt@uvic.ca>
2022-07-19 14:04:55 -0700

    Repository clean-up
    Adds more repo fixes including fixing typos, removing unused imports,
    and fixing formatting consistency.

--- PreProcessing/plotFeatures.py
+++ PreProcessing/plotFeatures.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn import preprocessing
 
 xAxisTicks = []
-for i in range(0,450,50):
+for i in range(0, 450, 50):
 	xAxisTicks.append(i)
 
 df = pd.read_json("./data/data_to_plot.json")
@@ -16,50 +16,50 @@ le.fit(df['semester'])
 df['semester'] = le.transform(df['semester'])
 
 plots = [
-	{	
-		"name":"# Offerings",
-		"filename":"plots/num_offerings.png"
+	{
+		"name": "# Offerings",
+		"filename": "plots/num_offerings.png"
 	},
-	{	
-		"name":"# prereqs",
-		"filename":"plots/num_prereqs.png"
+	{
+		"name": "# prereqs",
+		"filename": "plots/num_prereqs.png"
 	},
-	{	
-		"name":"# prereqs prev sem",
-		"filename":"plots/num_prereqs_prev_sem.png"
+	{
+		"name": "# prereqs prev sem",
+		"filename": "plots/num_prereqs_prev_sem.png"
 	},
-	{	
-		"name":"# students in prereqs",
-		"filename":"plots/num_students_in_prereqs.png"
+	{
+		"name": "# students in prereqs",
+		"filename": "plots/num_students_in_prereqs.png"
 	},
-	{	
-		"name":"semester",
-		"filename":"plots/semester.png"
+	{
+		"name": "semester",
+		"filename": "plots/semester.png"
 	},
-	{	
-		"name":"# Y1",
-		"filename":"plots/num_y1.png"
+	{
+		"name": "# Y1",
+		"filename": "plots/num_y1.png"
 	},
-	{	
-		"name":"# Y2",
-		"filename":"plots/num_y2.png"
+	{
+		"name": "# Y2",
+		"filename": "plots/num_y2.png"
 	},
-	{	
-		"name":"# Y3",
-		"filename":"plots/num_y3.png"
+	{
+		"name": "# Y3",
+		"filename": "plots/num_y3.png"
 	},
-	{	
-		"name":"# Y4",
-		"filename":"plots/num_y4.png"
+	{
+		"name": "# Y4",
+		"filename": "plots/num_y4.png"
 	},
-	{	
-		"name":"# Y5+",
-		"filename":"plots/num_y5+.png"
+	{
+		"name": "# Y5+",
+		"filename": "plots/num_y5+.png"
 	}
 ]
 
 for plot in plots:
-	fig, axes = plt.subplots(2,1)
+	fig, axes = plt.subplots(2, 1)
 	plt.subplots_adjust(top=0.9, bottom=0.1, hspace=0.2)
 
 	ndf = df.groupby(['capacity'])[plot["name"]].mean()
